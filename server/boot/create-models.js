b6fdfb6b65e07a64e2e716dc1fa4f4458f902369
Eng Wan Song <wansong@gmail.com>
2015-05-24 13:37:02 -0400

    Prevent data reset on restart.

--- server/boot/create-models.js
+++ server/boot/create-models.js
@@ -11,23 +11,31 @@ module.exports = function(app) {
 
   // create reviewers
   function createMovies(cb) {
-    mysqlDs.automigrate('Movie', function(err) {
+    mysqlDs.autoupdate('Movie', function(err) {
       if (err) return cb(err);
-      app.models.Movie.create([{
-        name: 'Bob',
-        year: 2015
-      }], cb);
+      app.models.Movie.find({where: {name:'Bob', year: 2015}}, function(err, movies) {
+        if (movies.length === 0) {
+          app.models.Movie.create([{
+            name: 'Bob',
+            year: 2015
+          }], cb);
+        }
+      });
     });
   }
 
   function createUsers(cb) {
-    mysqlDs.automigrate(['User', 'AccessToken', 'ACL'], function(err) {
+    mysqlDs.autoupdate(['User', 'AccessToken', 'ACL'], function(err) {
       if (err) return cb(err);
-      app.models.User.create([{
-        username: 'kahwee',
-        email: 'tengkahwee@gmail.com',
-        password: '!password1'
-      }], cb);
+      app.models.User.find({where: {username:'kahwee'}}, function(err, users) {
+        if (users.length === 0) {
+          app.models.User.create([{
+            username: 'kahwee',
+            email: 'tengkahwee@gmail.com',
+            password: '!password1'
+          }], cb);
+        } 
+      });
     });
   }
 };
